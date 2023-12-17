fddfba989156ac3e8893a936e38b025775738535
habibayoub <71305263+habibayoub@users.noreply.github.com>
2023-12-17 18:03:17 -0500

    Changes

--- 2023/day01/day01.cpp
+++ 2023/day01/day01.cpp
@@ -3,33 +3,26 @@
 #include <string>
 using namespace std;
 
+void processLine(const string& line, int& total) {
+    string number = "";
+    for (char c : line) {
+        if (isdigit(c)) {
+            number += c;
+        }
+    }
+    total += (number.length() == 1) ? stoi(number + number) : stoi(number.substr(0, 1) + "" + number.substr(number.length() - 1, 1));
+}
+
 int main() {
-    string line;
     int total = 0;
     ifstream file("input.txt");
     if (file.is_open()) {
+        string line;
         while (getline(file, line)) {
-            string number = "";
-            string stringnum = "";
-
-            for (int i = 0; i < line.length(); i++) {
-                if (isdigit(line[i])){
-                    number += line[i];
-                }
-            }
-            if (number.length() == 1){
-                stringnum = number + number;
-                total += stoi(stringnum);   
- 
-            } else {
-                stringnum = number.substr(0, 1) + "" + number.substr(number.length() - 1, 1);
-                total += stoi(stringnum);
-            }
-
-            
-            cout << total << "\n";
+            processLine(line, total);
         }
         file.close();
     }
+    cout << total << "\n";
     return 0;
 }
--- 2023/day01/day01p2.cpp
+++ 2023/day01/day01p2.cpp
@@ -3,44 +3,34 @@
 #include <string>
 using namespace std;
 
+void processLine(string& line, int& total) {
+
+    string spelled[] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    string numeric[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+    for (size_t i = 0; i < 9; i++){
+        size_t pos = line.find(spelled[i]);
+        while (pos != -1){
+            line.replace(pos, spelled[i].length(), numeric[i]);
+            pos = line.find(spelled[i]);
+        };
+    }
+
+    string number = "";
+    for (char c : line) {
+        if (isdigit(c)) {
+            number += c;
+        }
+    }
+    total += (number.length() == 1) ? stoi(number + number) : stoi(number.substr(0, 1) + "" + number.substr(number.length() - 1, 1));
+}
+
 int main() {
     string line;
     int total = 0;
     ifstream file("input.txt");
     if (file.is_open()) {
         while (getline(file, line)) {
-            string number = "";
-            string stringnum = "";
-            string spelled[] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-            string numeric[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
-            string test = "one2three4five";
-
-            for (size_t i = 0; i < 9; i++){
-                size_t pos = line.find(spelled[i]);
-                while (pos != -1){
-                    line.replace(pos, spelled[i].length(), numeric[i]);
-                    pos = line.find(spelled[i]);
-                };
-            }
-                //cout << line << "\n";
-            
-            for (int i = 0; i < line.length(); i++) {
-                if (isdigit(line[i])){
-                    number += line[i];
-                }
-            }
-            if (number.length() == 1){
-                stringnum = number + number;
-                total += stoi(number + number);   
-                cout << stringnum << "\n";
- 
-            } else {
-                stringnum = number.substr(0, 1) + "" + number.substr(number.length() - 1, 1);
-                total += stoi(number.substr(0, 1) + "" + number.substr(number.length() - 1, 1));
-                cout << stringnum << "\n";
-            }
-
-            
+            processLine(line, total);
         }
         file.close();
     }
